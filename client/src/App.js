1225f85af1a990abc40794df7042517203e08cd7
Janez <janez.cadez007@gmail.com>
2025-04-08 09:31:58 -0600

    client change + app js formatting + try out other routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   state = {
-    method: 'GET',
-    lastRequest: '',
+    method: "GET",
+    lastRequest: "",
 
-    id: '',
-    title: '',
-    order: '',
+    id: "",
+    title: "",
+    order: "",
     completed: false,
 
     response: [],
   };
-  
-  handleSubmit = async e => {
+
+  handleSubmit = async (e) => {
     e.preventDefault();
     let { method, id, title, order, completed } = this.state;
-    
+
     let request = {
       method,
       headers: {
-        'Content-Type': 'application/json',
-      }
+        "Content-Type": "application/json",
+      },
     };
 
     // Undefined ensures not changing to empty string.
@@ -33,25 +33,29 @@ class App extends Component {
     order = order ? Number(order) : undefined;
 
     if (method !== "GET")
-      request.body = JSON.stringify({ title, order, completed })
+      request.body = JSON.stringify({ title, order, completed });
 
-    this.setState({ lastRequest: `${method} at /${id}`});
+    this.setState({ lastRequest: `${method} at /${id}` });
     // Code smells, but the setup of todo-backend with get('/') returning a list of todos requires
     // that we directly hit localhost instead of being able to rely on the proxy.
     // We can only proxy non-root gets.
     let response;
-    if (process.env.NODE_ENV === "development" && method === "GET" && id === '') {
-      response = await fetch('http://localhost:5000/', request);
+    if (
+      process.env.NODE_ENV === "development" &&
+      method === "GET" &&
+      id === ""
+    ) {
+      response = await fetch("http://localhost:5001/users", request);
     } else {
       response = await fetch(`/${id}`, request);
     }
 
-    const contentType = response.headers.get('content-type');
+    const contentType = response.headers.get("content-type");
 
     let body;
-    if (contentType && contentType.includes('application/json')) {
+    if (contentType && contentType.includes("application/json")) {
       body = await response.json();
-    } else if (contentType && contentType.includes('text/html')) {
+    } else if (contentType && contentType.includes("text/html")) {
       body = await response.text();
     }
 
@@ -62,18 +66,18 @@ class App extends Component {
     }
 
     // Ensures formart of [{}, {}, {}]
-    if (!Array.isArray(body))
-      body = Array(body);
-  
+    if (!Array.isArray(body)) body = Array(body);
+
     this.setState({ response: body });
   };
 
-  changeMethod = event => {
+  changeMethod = (event) => {
     this.setState({ method: event.target.value });
   };
-  
+
   render() {
-    const { method, lastRequest, id, title, order, completed, response } = this.state;
+    const { method, lastRequest, id, title, order, completed, response } =
+      this.state;
 
     const shouldDisplayIdInput = method !== "POST";
     const shouldDisplayTitleInput = method === "POST" || method === "PATCH";
@@ -84,9 +88,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Powered by React
-          </p>
+          <p>Powered by React</p>
         </header>
 
         <form onSubmit={this.handleSubmit}>
@@ -104,21 +106,21 @@ class App extends Component {
             type="text"
             placeholder="id (int)"
             value={id}
-            onChange={e => this.setState({ id: e.target.value })}
+            onChange={(e) => this.setState({ id: e.target.value })}
           />
           <input
             disabled={!shouldDisplayTitleInput}
             type="text"
             placeholder="title (string)"
             value={title}
-            onChange={e => this.setState({ title: e.target.value })}
+            onChange={(e) => this.setState({ title: e.target.value })}
           />
           <input
             disabled={!shouldDisplayOrderInput}
             type="text"
             placeholder="order (int)"
             value={order}
-            onChange={e => this.setState({ order: e.target.value })}
+            onChange={(e) => this.setState({ order: e.target.value })}
           />
 
           <label>
@@ -128,7 +130,7 @@ class App extends Component {
               disabled={!shouldDisplayCompletedInput}
               type="checkbox"
               value={completed}
-              onChange={e => this.setState({ completed: e.target.checked })}
+              onChange={(e) => this.setState({ completed: e.target.checked })}
             />
           </label>
 
@@ -136,19 +138,17 @@ class App extends Component {
         </form>
         <h3>{`Last sent: ${lastRequest}`}</h3>
         <p>
-          {
-            response.map((todo, i) => {
-              return (
-                <li key={i}>
-                  { 
-                    todo ? Object.entries(todo).map(([key, value]) => {
-                      return `${key}: ${value}   `
-                    }) : undefined
-                  }
-                </li>
-              )
-            })
-          }
+          {response.map((todo, i) => {
+            return (
+              <li key={i}>
+                {todo
+                  ? Object.entries(todo).map(([key, value]) => {
+                      return `${key}: ${value}   `;
+                    })
+                  : undefined}
+              </li>
+            );
+          })}
         </p>
       </div>
     );
