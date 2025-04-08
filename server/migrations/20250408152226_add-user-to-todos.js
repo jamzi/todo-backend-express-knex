c57b1f1c149331337bf6f3e331e4835aa4f430c4
Janez <janez.cadez007@gmail.com>
2025-04-08 09:33:04 -0600

    added userId to the todos schema migration

---
+++ server/migrations/20250408152226_add-user-to-todos.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable("todos", function (table) {
+    table.integer("userId");
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable("todos", function (table) {
+    table.dropColumn("userId");
+  });
+};
