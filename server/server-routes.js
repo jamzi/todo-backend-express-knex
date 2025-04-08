547905ebfad1c642e9973be57811f54b15ab69fe
Janez <janez.cadez007@gmail.com>
2025-04-08 09:32:18 -0600

    added the users route helpers and endpoints

--- server/server-routes.js
+++ server/server-routes.js
@@ -1,22 +1,34 @@
-const _ = require('lodash');
-const todos = require('./database/todo-queries.js');
+const _ = require("lodash");
+const todos = require("./database/todo-queries.js");
+const users = require("./database/user-queries.js");
 
 function createToDo(req, data) {
-  const protocol = req.protocol, 
-    host = req.get('host'), 
+  const protocol = req.protocol,
+    host = req.get("host"),
     id = data.id;
 
   return {
     title: data.title,
     order: data.order,
     completed: data.completed || false,
-    url: `${protocol}://${host}/${id}`
+    url: `${protocol}://${host}/${id}`,
+  };
+}
+
+function createUser(req, data) {
+  const protocol = req.protocol,
+    host = req.get("host"),
+    id = data.id;
+
+  return {
+    name: data.name,
+    url: `${protocol}://${host}/${id}`,
   };
 }
 
 async function getAllTodos(req, res) {
   const allEntries = await todos.all();
-  return res.send(allEntries.map( _.curry(createToDo)(req) ));
+  return res.send(allEntries.map(_.curry(createToDo)(req)));
 }
 
 async function getTodo(req, res) {
@@ -36,7 +48,7 @@ async function patchTodo(req, res) {
 
 async function deleteAllTodos(req, res) {
   const deletedEntries = await todos.clear();
-  return res.send(deletedEntries.map( _.curry(createToDo)(req) ));
+  return res.send(deletedEntries.map(_.curry(createToDo)(req)));
 }
 
 async function deleteTodo(req, res) {
@@ -44,30 +56,99 @@ async function deleteTodo(req, res) {
   return res.send(createToDo(req, deleted));
 }
 
-function addErrorReporting(func, message) {
-    return async function(req, res) {
-        try {
-            return await func(req, res);
-        } catch(err) {
-            console.log(`${message} caused by: ${err}`);
+async function getAllUsers(req, res) {
+  const allEntries = await users.all();
+  return res.send(allEntries.map(_.curry(createUser)(req)));
+}
 
-            // Not always 500, but for simplicity's sake.
-            res.status(500).send(`Opps! ${message}.`);
-        } 
+async function getUser(req, res) {
+  const user = await users.get(req.params.id);
+  return res.send(user);
+}
+
+async function postUser(req, res) {
+  const created = await users.create(req.body.name);
+  return res.send(createUser(req, created));
+}
+
+async function patchUser(req, res) {
+  const patched = await users.update(req.params.id, req.body);
+  return res.send(createUser(req, patched));
+}
+
+async function deleteUser(req, res) {
+  const deleted = await users.delete(req.params.id);
+  return res.send(createUser(req, deleted));
+}
+
+async function deleteAllUsers(req, res) {
+  const deletedEntries = await users.clear();
+  return res.send(deletedEntries.map(_.curry(createUser)(req)));
+}
+
+async function assignUserToTodo(req, res) {
+  const assigned = await todos.assignUserToTodo(req.params.id, req.body.todoId);
+  return res.send(createToDo(req, assigned));
+}
+
+async function unassignUserFromTodo(req, res) {
+  const unassigned = await todos.unassignUserFromTodo(req.body.todoId);
+  return res.send(createToDo(req, unassigned));
+}
+
+function addErrorReporting(func, message) {
+  return async function (req, res) {
+    try {
+      return await func(req, res);
+    } catch (err) {
+      console.log(`${message} caused by: ${err}`);
+
+      // Not always 500, but for simplicity's sake.
+      res.status(500).send(`Opps! ${message}.`);
     }
+  };
 }
 
 const toExport = {
-    getAllTodos: { method: getAllTodos, errorMessage: "Could not fetch all todos" },
-    getTodo: { method: getTodo, errorMessage: "Could not fetch todo" },
-    postTodo: { method: postTodo, errorMessage: "Could not post todo" },
-    patchTodo: { method: patchTodo, errorMessage: "Could not patch todo" },
-    deleteAllTodos: { method: deleteAllTodos, errorMessage: "Could not delete all todos" },
-    deleteTodo: { method: deleteTodo, errorMessage: "Could not delete todo" }
-}
+  getAllTodos: {
+    method: getAllTodos,
+    errorMessage: "Could not fetch all todos",
+  },
+  getTodo: { method: getTodo, errorMessage: "Could not fetch todo" },
+  postTodo: { method: postTodo, errorMessage: "Could not post todo" },
+  patchTodo: { method: patchTodo, errorMessage: "Could not patch todo" },
+  deleteAllTodos: {
+    method: deleteAllTodos,
+    errorMessage: "Could not delete all todos",
+  },
+  deleteTodo: { method: deleteTodo, errorMessage: "Could not delete todo" },
+  getAllUsers: {
+    method: getAllUsers,
+    errorMessage: "Could not fetch all users",
+  },
+  getUser: { method: getUser, errorMessage: "Could not fetch user" },
+  postUser: { method: postUser, errorMessage: "Could not post user" },
+  patchUser: { method: patchUser, errorMessage: "Could not patch user" },
+  deleteAllUsers: {
+    method: deleteAllUsers,
+    errorMessage: "Could not delete all users",
+  },
+  deleteUser: { method: deleteUser, errorMessage: "Could not delete user" },
+  assignUserToTodo: {
+    method: assignUserToTodo,
+    errorMessage: "Could not assign user to todo",
+  },
+  unassignUserFromTodo: {
+    method: unassignUserFromTodo,
+    errorMessage: "Could not unassign user from todo",
+  },
+};
 
 for (let route in toExport) {
-    toExport[route] = addErrorReporting(toExport[route].method, toExport[route].errorMessage);
+  toExport[route] = addErrorReporting(
+    toExport[route].method,
+    toExport[route].errorMessage
+  );
 }
 
 module.exports = toExport;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,30 @@
-const app = require('./server-config.js');
-const routes = require('./server-routes.js');
+const app = require("./server-config.js");
+const routes = require("./server-routes.js");
 
 const port = process.env.PORT || 5000;
 
-app.get('/', routes.getAllTodos);
-app.get('/:id', routes.getTodo);
+app.get("/todos", routes.getAllTodos);
+app.get("/todos/:id", routes.getTodo);
 
-app.post('/', routes.postTodo);
-app.patch('/:id', routes.patchTodo);
+app.post("/todos", routes.postTodo);
+app.patch("/todos/:id", routes.patchTodo);
 
-app.delete('/', routes.deleteAllTodos);
-app.delete('/:id', routes.deleteTodo);
+app.delete("/todos", routes.deleteAllTodos);
+app.delete("/todos/:id", routes.deleteTodo);
 
-if (process.env.NODE_ENV !== 'test') {
+app.post("/todos/users/:id/assign", routes.assignUserToTodo);
+app.post("/todos/users/:id/unassign", routes.unassignUserFromTodo);
+
+app.get("/users", routes.getAllUsers);
+app.get("/users/:id", routes.getUser);
+
+app.post("/users", routes.postUser);
+app.patch("/users/:id", routes.patchUser);
+
+app.delete("/users", routes.deleteAllUsers);
+app.delete("/users/:id", routes.deleteUser);
+
+if (process.env.NODE_ENV !== "test") {
   app.listen(port, () => console.log(`Listening on port ${port}`));
 }
 
